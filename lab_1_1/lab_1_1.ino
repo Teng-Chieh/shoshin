f6c8f37152b56629fd0f1a91ea465265557509ca
Ryan Chan <tengc.chan@gmail.com>
2022-05-01 14:20:47 +0800

    [Refactor] comment lab1_1

--- lab_1_1/lab_1_1.ino
+++ lab_1_1/lab_1_1.ino
@@ -1,13 +1,14 @@
+/* 開機只會執行一次 */
 void setup()
 {
-    pinMode(13, OUTPUT);    // sets the digital pin 13 as output
+    pinMode(13, OUTPUT);    // sets the digital pin 13 as output (設定PIN13這根腳為Output Mode)
 }
 
+/* 重複執行此區段 */
 void loop()
 {
-    digitalWrite(13, HIGH); // sets the digital pin 13 on
-    delay(1000);            // waits for a second
-    digitalWrite(13, LOW);  // sets the digital pin 13 off
-    delay(1000);            // waits for a second
+    digitalWrite(13, HIGH); // sets the digital pin 13 on     (設定PIN13這根腳為輸出HIGH)
+    delay(1000);            // waits for a second             (延遲1秒 -> 單位為ms, 1s = 1000ms)  
+    digitalWrite(13, LOW);  // sets the digital pin 13 off    (設定PIN13這根腳為輸出LOW)
+    delay(1000);            // waits for a second             (延遲1秒 -> 單位為ms, 1s = 1000ms)
 }
-
